bf2d1cdb6f569b8d7443d21c37889cdbea7e757f
David Davidson <daviddav@textio.com>
2020-05-28 07:59:41 -0700

    Tidy up web-worker interface with channel

--- src/main/chess/main.cljs
+++ src/main/chess/main.cljs
@@ -1,11 +1,39 @@
 (ns chess.main
     (:require [reagent.core :as reagent]
+              [cljs.core.async :refer [chan put! take! <! go-loop]]
               [chess.components :refer [app]]
               [chess.utils :refer [reverse-color]]
               [chess.moves :refer [is-check? is-checkmate?]]
               [chess.board :refer [get-initial-board move-piece]]))
 
-(def web-worker (js/Worker. "/compiled/worker.js"))
+(declare set-piece)
+
+; Side effects: initializes worker and sets up messaging
+(defn get-worker-channels []
+    (let [web-worker (js/Worker. "/compiled/worker.js")
+          from-worker (chan)
+          to-worker (chan)]
+
+        (.. web-worker (addEventListener "message" (fn [evt]
+            (let [move-data (js->clj (.. evt -data))]
+                (put! from-worker move-data)))))
+
+        (go-loop []
+            (let [val (<! to-worker)]
+                (when val (.. web-worker (postMessage (clj->js val)))))
+            (recur))
+
+        [from-worker to-worker]))
+
+(defonce worker-channels (get-worker-channels))
+
+(defn message-worker [board color search-depth]
+    (let [[from-worker to-worker] worker-channels]
+        (js/Promise. (fn [resolve]
+            (put! to-worker {:board board
+                             :color color
+                             :search-depth search-depth})
+            (take! from-worker resolve)))))
 
 (defonce search-depth (reagent/atom 4))
 (defn set-search-depth [depth] (reset! search-depth depth))
@@ -17,33 +45,40 @@
 (defn set-hovered-coords [coords] (reset! hovered-coords coords))
 
 (defonce active-color (reagent/atom "white"))
-(defn set-active-player [player] (reset! active-color player))
+(defn set-active-color [color] (reset! active-color color))
+
+(defn init-next-move []
+    (when (= @active-color "black")
+          (-> (message-worker @board @active-color @search-depth)
+              (.then (fn [[from to]]
+                (set-piece @active-color from to))))))
+
+(defn alert-check-checkmate [new-board from-color]
+    (js/setTimeout (fn []
+        (when (is-check? new-board (reverse-color from-color))
+              (if (is-checkmate? new-board (reverse-color from-color))
+                  (js/alert "Checkmate")
+                  (js/alert "Check")))) 20)) ; 20ms to give time for board to render latest move
 
 (defn set-piece [from-color from to]
     (let [new-board (move-piece @board from to)
-          new-player (reverse-color from-color)]
-        (set-active-player new-player)
+          new-color (reverse-color from-color)]
+        (set-active-color new-color)
         (set-board new-board)
-        (js/setTimeout (fn []
-            (when (is-check? new-board (reverse-color from-color))
-                (if (is-checkmate? new-board (reverse-color from-color))
-                    (js/alert "Checkmate")
-                    (js/alert "Check")))) 100)
-        (when (= @active-color "black")
-              (.. web-worker (postMessage (clj->js {:board @board
-                                                    :color @active-color
-                                                    :search-depth @search-depth}))))))
-
-(.. web-worker (addEventListener "message" (fn [evt]
-    (let [[from to] (js->clj (.. evt -data))]
-        (set-piece @active-color from to)))))
+        (alert-check-checkmate new-board from-color)
+        (init-next-move)))
 
 ; --------------------------------------------------------------------------------------------------
 
 (defn mount! []
-    (reagent/render [app board hovered-coords active-color search-depth {:set-hovered-coords set-hovered-coords
-                                                                         :set-piece set-piece
-                                                                         :set-search-depth set-search-depth}]
+    (reagent/render [app
+                     board
+                     hovered-coords
+                     active-color
+                     search-depth
+                     {:set-hovered-coords set-hovered-coords
+                      :set-piece set-piece
+                      :set-search-depth set-search-depth}]
                     (.getElementById js/document "app")))
 
 (defn load! []
